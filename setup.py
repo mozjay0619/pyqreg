ad33ae652c4866e6d95a11bc89725976b3102bd2
Jay Kim <jkim2@grubhub.com>
2021-09-07 20:35:36 -0500

    0.0.b13 final comments

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ else:
     ]
 
 # circleci.py version
-VERSION = "v0.0.b12"
+VERSION = "v0.0.b13"
 
 # circleci version verfication
 class VerifyVersionCommand(install):
--- src/pyqreg/quantreg.py
+++ src/pyqreg/quantreg.py
@@ -10,7 +10,16 @@ class QuantReg():
 		self.y = np.array(y, np.double, copy=False, order='F', ndmin=1)
 
 	def fit_ipm(self, q, eps=1e-6):
+		"""Estimate coefficients using the interior point method.
 
+		Paramters
+		---------
+		q : double
+			Quantile value strictly between 0 and 1
+
+		eps : double
+			Duality gap stopping criterion
+		"""
 		coefs = fit_coefs(self.X, self.y, q, eps)
 
 		return coefs
