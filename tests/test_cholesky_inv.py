fa5e856e83767c8ddad1a06e688d61a23b8a2d43
Jay Kim <jkim2@grubhub.com>
2021-09-09 01:02:09 -0500

    0.3.2 bug fix

--- tests/test_cholesky_inv.py
+++ tests/test_cholesky_inv.py
@@ -9,10 +9,10 @@ from numpy.linalg import inv
 def test_cholesky_inv():
 
 	rng = rng_generator(1)
+	n = 100
 
-	A = rng.normal(size=[matrixSize, matrixSize])
+	A = rng.normal(size=[n, n])
 	B = np.dot(A, A.transpose())
 	B = np.array(B, np.double, copy=False, order='F', ndmin=1)
 
 	assert np.all(np.close(lapack_cholesky_inv(B), inv(B)))
-	
